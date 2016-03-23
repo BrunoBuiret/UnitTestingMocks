cd3bc56df28cd0fe29851339bde590df60b692f2
Bruno Buiret <bruno.b@apps-panel.com>
2016-03-23 12:40:29 +0100

    Mercredi 23/03

---
+++ test/persons/TestPerson.java
@@ -0,0 +1,21 @@
+package persons;
+
+import org.junit.Test;
+
+/**
+ * @author Bruno Buiret <bruno.buiret@etu.univ-lyon1.fr>
+ */
+public class TestPerson
+{
+    @Test
+    public void testWasBorn()
+    {
+        
+    }
+    
+    @Test
+    public void testGetAge()
+    {
+        
+    }
+}
