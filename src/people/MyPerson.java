6c94c9868116bcd315f4baded4efda86a280b7c7
Bruno Buiret <Bruno Buiret>
2016-03-28 20:27:36 +0200

    Lundi 28/03

---
+++ src/people/MyPerson.java
@@ -0,0 +1,76 @@
+package people;
+
+import java.util.GregorianCalendar;
+import persons.IPerson;
+
+/**
+ * @author Bruno Buiret (bruno.buiret@etu.univ-lyon1.fr)
+ */
+public class MyPerson implements IPerson
+{
+    /**
+     * A reference to the actual person.
+     */
+    protected Personne internalPerson;
+    
+    /**
+     * Creates a new person.
+     * 
+     * @param lastName The person's last name.
+     * @param firstName The person's first name.
+     * @param year The person's year of birth.
+     * @param month The person's month of birth.
+     * @param day The person's day of birth.
+     */
+    public MyPerson(String lastName, String firstName, int year, int month, int day)
+    {
+        this.internalPerson = new Personne(lastName, firstName, year, month, day);
+    }
+    
+    /**
+     * Gets the person's last name.
+     * 
+     * @return The person's last name.
+     */
+    @Override
+    public String getName()
+    {
+        return this.internalPerson.getName();
+    }
+
+    /**
+     * Gets the person's first name.
+     * 
+     * @return The person's first name.
+     */
+    @Override
+    public String getFirstName()
+    {
+        return this.internalPerson.getFirstName();
+    }
+
+    /**
+     * Tests if a person was already born on <code>date</code>.
+     * 
+     * @param date The date to test.
+     * @return <code>true</code> if the person was already born, <code>false</code> otherwise.
+     * @todo Implement this method.
+     */
+    @Override
+    public boolean wasBorn(GregorianCalendar date)
+    {
+        return false;
+    }
+
+    /**
+     * Computes the person's age on a given date from their birth date.
+     * 
+     * @param date The date of reference.
+     * @return The person's age.
+     */
+    @Override
+    public int getAge(GregorianCalendar date)
+    {
+        return this.internalPerson.getAge(date);
+    }
+}
--- test/persons/TestPerson.java
+++ test/persons/TestPerson.java
@@ -1,21 +1,143 @@
 package persons;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+import junit.framework.AssertionFailedError;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import people.EfficientPerson;
+import people.OneMorePerson;
+import people.SimplePerson;
+import people.SmallCodePerson;
+import people.YetAnotherPerson;
 
 /**
- * @author Bruno Buiret <bruno.buiret@etu.univ-lyon1.fr>
+ * @author Bruno Buiret (bruno.buiret@etu.univ-lyon1.fr)
  */
+@RunWith(Parameterized.class)
 public class TestPerson
 {
+    /**
+     * The first parameter, a reference to a person.
+     */
+    @Parameter(0)
+    public IPerson personParameter;
+
+    /**
+     * The second parameter, a reference to a calendar
+     */
+    @Parameter(1)
+    public GregorianCalendar calendarParameter;
+    
+    /**
+     * 
+     */
+    @Parameter(2)
+    public boolean supposedToSucceedParameter;
+
+    /**
+     *
+     */
+    protected DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
+    /**
+     *
+     * @return
+     */
+    @Parameters
+    public static Iterable<Object[]> createParameters()
+    {
+        // Initialize vars
+        List<Object[]> testValues = new ArrayList<>();
+        List<IPerson> persons = new ArrayList<>();
+        List<GregorianCalendar> calendars = new ArrayList<>();
+        
+        // Build persons
+        GregorianCalendar birthDate = new GregorianCalendar(1994, 0, 21);
+        persons.add(new Person("Buiret", "Bruno", birthDate));
+        persons.add(new SimplePerson("Buiret", "Bruno", 1994, 1, 21));
+        persons.add(new SmallCodePerson("Buiret", "Bruno", 1994, 1, 21));
+        persons.add(new YetAnotherPerson("Buiret", "Bruno", 1994, 1, 21));
+        persons.add(new OneMorePerson("Buiret", "Bruno", 1994, 1, 21));
+        persons.add(new EfficientPerson("Buiret", "Bruno", 22));
+        
+        // Build calendars
+        calendars.add(new GregorianCalendar(1994, 0, 21));
+        calendars.add(new GregorianCalendar(2016, 2, 26));
+        calendars.add(new GregorianCalendar(1994, 0, 1));
+        
+        // Build tests
+        persons.stream().forEach((person) ->
+        {
+            calendars.stream().forEach((calendar) ->
+            {
+                testValues.add(new Object[]{person, calendar, calendar.compareTo(birthDate) >= 0});
+            });
+        });
+
+        return testValues;
+    }
+
+    /**
+     * Runs tests on method <code>IPerson.wasBorn()</code>.
+     *
+     * @see persons.IPerson#wasBorn(java.util.GregorianCalendar)
+     */
     @Test
     public void testWasBorn()
     {
-        
+        if(this.supposedToSucceedParameter)
+        {
+            Assert.assertTrue(
+                String.format(
+                    "%s.wasBorn(\"%s\") is supposed to be true.",
+                    this.personParameter.getClass().getSimpleName(),
+                    this.dateFormat.format(this.calendarParameter.getTime())
+                ),
+                this.personParameter.wasBorn(this.calendarParameter)
+            );
+        }
+        else
+        {
+            Assert.assertFalse(
+                String.format(
+                    "%s.wasBorn(\"%s\") is supposed to be false.",
+                    this.personParameter.getClass().getSimpleName(),
+                    this.dateFormat.format(this.calendarParameter.getTime())
+                ),
+                this.personParameter.wasBorn(this.calendarParameter)
+            );
+        }
     }
-    
+
+    /**
+     * Runs tests on method <code>IPerson.getAge()</code>.
+     *
+     * @see persons.IPerson#getAge(java.util.GregorianCalendar)
+     */
     @Test
     public void testGetAge()
     {
-        
+        try
+        {
+            this.personParameter.getAge(this.calendarParameter);
+        }
+        catch(IllegalArgumentException ex)
+        {
+            if(this.supposedToSucceedParameter)
+            {
+                AssertionFailedError error = new AssertionFailedError("The person wasn't born yet.");
+                error.addSuppressed(ex);
+
+                throw error;
+            }
+        }
     }
 }
